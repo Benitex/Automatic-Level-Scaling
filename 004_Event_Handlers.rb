aab17a723bcfb0ca7ce9541e8d6e5519f3e19c26
Benitex <benitoapepe@yahoo.com.br>
2023-11-03 14:13:19 -0300

    Fix settings reset after the first setNewLevel of a trainer

--- 004_Event_Handlers.rb
+++ 004_Event_Handlers.rb
@@ -7,36 +7,40 @@
 Events.onWildPokemonCreate += proc { |_sender, e|
   pokemon = e[0]
   id = pbGet(LevelScalingSettings::WILD_VARIABLE)
-  if id != 0
-    AutomaticLevelScaling.setDifficulty(id)
-    AutomaticLevelScaling.setNewLevel(pokemon)
-  end
+  next if id == 0
+  AutomaticLevelScaling.setDifficulty(id)
+
+  AutomaticLevelScaling.setNewLevel(pokemon)
+  AutomaticLevelScaling.setSettings if AutomaticLevelScaling.settings[:temporary] # reset settings
 }
 
 # Activates script when a trainer pokemon is created
 Events.onTrainerPartyLoad += proc { |_sender, trainer|
   id = pbGet(LevelScalingSettings::TRAINER_VARIABLE)
-  if trainer && id != 0
-    AutomaticLevelScaling.setDifficulty(id)
-    avarage_level = 0
-    trainer[0].party.each { |pokemon| avarage_level += pokemon.level }
-    avarage_level /= trainer[0].party.length
+  next if !trainer || id == 0
+  AutomaticLevelScaling.setDifficulty(id)
 
-    for pokemon in trainer[0].party
-      AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
-    end
+  avarage_level = 0
+  trainer[0].party.each { |pokemon| avarage_level += pokemon.level }
+  avarage_level /= trainer[0].party.length
+
+  for pokemon in trainer[0].party
+    AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
   end
+  AutomaticLevelScaling.setSettings if AutomaticLevelScaling.settings[:temporary] # reset settings
 }
 
 # Updates partner's pokemon levels after battle
 Events.onEndBattle += proc { |_sender,e|
-  if $PokemonGlobal.partner != nil
-    avarage_level = 0
-    $PokemonGlobal.partner[3].each { |pokemon| avarage_level += pokemon.level }
-    avarage_level /= $PokemonGlobal.partner[3].length
+  id = pbGet(LevelScalingSettings::TRAINER_VARIABLE)
+  next if !$PokemonGlobal.partner || id == 0
 
-    for pokemon in $PokemonGlobal.partner[3] do
-      AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
-    end
+  avarage_level = 0
+  $PokemonGlobal.partner[3].each { |pokemon| avarage_level += pokemon.level }
+  avarage_level /= $PokemonGlobal.partner[3].length
+
+  for pokemon in $PokemonGlobal.partner[3] do
+    AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
   end
+  AutomaticLevelScaling.setSettings if AutomaticLevelScaling.settings[:temporary] # reset settings
 }
