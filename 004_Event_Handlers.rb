4efd69e345fdad37d92bc5a74c0b008ca1322eae
Benitex <benitoapepe@yahoo.com.br>
2024-12-13 11:25:41 -0300

    Add save_trainer_parties and use_map_level_for_wild_pokemon settings

--- 004_Event_Handlers.rb
+++ 004_Event_Handlers.rb
@@ -9,7 +9,12 @@ Events.onWildPokemonCreate += proc { |_sender, e|
   id = pbGet(LevelScalingSettings::WILD_VARIABLE)
   next if id == 0
   AutomaticLevelScaling.difficulty = id
-  AutomaticLevelScaling.setNewLevel(pokemon)
+
+  if AutomaticLevelScaling.settings[:use_map_level_for_wild_pokemon]
+    pokemon.scale(AutomaticLevelScaling.getMapLevel($game_map.map_id))
+  else
+    pokemon.scale
+  end
 }
 
 # Activates script when a trainer pokemon is created
@@ -17,13 +22,28 @@ Events.onTrainerPartyLoad += proc { |_sender, trainer|
   id = pbGet(LevelScalingSettings::TRAINER_VARIABLE)
   next if !trainer || id == 0
   AutomaticLevelScaling.difficulty = id
+  trainer = trainer[0]
+
+  if AutomaticLevelScaling.settings[:save_trainer_parties] && AutomaticLevelScaling.battledTrainer?(trainer.key)
+    AutomaticLevelScaling.scaleToPreviousTrainerParty(trainer)
+    next
+  end
 
   avarage_level = 0
-  trainer[0].party.each { |pokemon| avarage_level += pokemon.level }
-  avarage_level /= trainer[0].party.length
+  trainer.party.each { |pokemon| avarage_level += pokemon.level }
+  avarage_level /= trainer.party.length
 
-  for pokemon in trainer[0].party
-    AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
+  for pokemon in trainer.party do
+    if AutomaticLevelScaling.settings[:proportional_scaling]
+      difference_from_average = pokemon.level - avarage_level
+      pokemon.scale(AutomaticLevelScaling.getScaledLevel + difference_from_average)
+    else
+      pokemon.scale
+    end
+  end
+
+  if AutomaticLevelScaling.settings[:save_trainer_parties]
+    AutomaticLevelScaling.savePreviousTrainerParty(trainer.key, trainer.party)
   end
 }
 
@@ -37,7 +57,12 @@ Events.onEndBattle += proc { |_sender, e|
   avarage_level /= $PokemonGlobal.partner[3].length
 
   for pokemon in $PokemonGlobal.partner[3] do
-    AutomaticLevelScaling.setNewLevel(pokemon, pokemon.level - avarage_level)
+    if AutomaticLevelScaling.settings[:proportional_scaling]
+      difference_from_average = pokemon.level - avarage_level
+      pokemon.scale(AutomaticLevelScaling.getScaledLevel + difference_from_average)
+    else
+      pokemon.scale
+    end
   end
 }
 
@@ -46,4 +71,11 @@ Events.onEndBattle += proc { |_sender, e|
   if AutomaticLevelScaling.settings[:temporary]
     AutomaticLevelScaling.setSettings
   end
+}
+
+#  Set map level when player enters a map
+Events.onMapChange += proc { |_sender, e|
+  next if !AutomaticLevelScaling.settings[:use_map_level_for_wild_pokemon]
+  next if $PokemonGlobal.map_levels.has_key?($game_map.map_id)
+  $PokemonGlobal.map_levels[$game_map.map_id] = AutomaticLevelScaling.getScaledLevel
 }
